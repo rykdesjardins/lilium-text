d341634046f9bb72d243b012e08c4eef69d0211e
Erik Desjardins <erik.desjardins@gmail.com>
2018-02-14 07:23:14 -0500

    Removed unused dev files

--- testserver.js
+++ testserver.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app.set('port', 3000);
-app.use(express.static('dev'));
+app.use(express.static(process.argv[2] || 'dev'));
 app.listen(app.get('port'));
